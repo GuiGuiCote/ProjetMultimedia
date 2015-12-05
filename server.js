be70d1759d443a20e8fdd8aecef1c71cc25c31f0
GuiGuiCote <guillaume.cote@outlook>
2015-12-05 17:07:51 +0100

    Correction bug suite migration socket io v0.9 to v1.0

--- server.js
+++ server.js
@@ -4,36 +4,35 @@ var gameport = 4004,
     UUID = require('node-uuid'),
     app = express(),
     server = http.createServer(app),
-    io = require('socket.io');
+    io = require('socket.io')(server);
 
 ///////EXPRESS
 server.listen(gameport);
 console.log('\t :: Express :: Listening on port ' + gameport );
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get( '/*' , function( req, res) {
     var file = req.params[0];
-    res.sendfile(__dirname + '/' + file );
+    res.sendFile(__dirname + '/' + file );
 });
 
 ////////SOCKET.IO
-var sio = io.listen(server);
-sio.configure(function(){
-    sio.set('log', 0);
-    sio.set('auth', function(handS, callback){
-        callback(null, true);
-    });
+io.use(function(socket, next){
+    console.log('Function use called');
+    var handShakeData = socket.request;
+    next();
 });
 
-sio.sockets.on('connection', function(client){
-    client.userid = UUID.v1();
-    client.emit('onconnected', {id: client.userid});
-    console.log('\t socket.io:: player ' + client.userid + ' connected');
-    client.on('disconnect', function(){
-        console.log('\t socket.io:: client disconnected ' + client.userid );
+io.sockets.on('connection', function(player){
+    console.log('Sockets called');
+    player.userid = UUID.v1();
+    player.emit('onconnected', {id: player.userid});
+    console.log('\t socket.io:: player ' + player.userid + ' connected');
+    player.on('disconnect', function(){
+        console.log('\t socket.io:: player disconnected ' + player.userid );
     })
 });
 
